e35227a2b5e454fa09f7dd462bba7a45cc18a417
Adrian Salceanu <e@essenciary.com>
2019-06-20 22:59:16 +0200

    installer fix

--- src/HelloPlugin.jl
+++ src/HelloPlugin.jl
@@ -2,12 +2,12 @@ module HelloPlugin
 
 using Genie
 
-function install(dest::String)
+function install(dest::String; force = false)
   src = abspath(normpath(joinpath(@__DIR__, "..", Genie.Plugins.FILES_FOLDER)))
 
   for f in readdir(src)
     isdir(f) || continue
-    Genie.Plugins.install(joinpath(src, f), dest)
+    Genie.Plugins.install(joinpath(src, f), dest, force = force)
   end
 end
 
